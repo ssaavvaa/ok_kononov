597f8411037ed8fc7bbed9c0d214c0ca13ef2e5a
Anton <toxasava@icloud.com>
2019-07-05 07:15:59 -0400

    Updates

---
+++ component---src-pages-store-index-js-b423118e933414e2eb15.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{160:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),r=n(169),l=n(168);t.default=function(){return c.a.createElement(r.a,null,c.a.createElement(l.a,{title:"Instructor"}),c.a.createElement("div",{className:"container",style:{minHeight:"100vh"}},c.a.createElement("h1",{style:{textAlign:"center"}},"Тут будет страница с товаром и всякими штучками")))}}}]);
+//# sourceMappingURL=component---src-pages-store-index-js-b423118e933414e2eb15.js.map
---
+++ styles-c9e6243fdc0ee2fe4614.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{223:function(n,o,c){},224:function(n,o,c){},225:function(n,o,c){},226:function(n,o,c){},227:function(n,o,c){},228:function(n,o,c){},229:function(n,o,c){},230:function(n,o,c){},231:function(n,o,c){},232:function(n,o,c){},233:function(n,o,c){},234:function(n,o,c){},235:function(n,o,c){},236:function(n,o,c){},237:function(n,o,c){},238:function(n,o,c){},239:function(n,o,c){},240:function(n,o,c){},241:function(n,o,c){}}]);
+//# sourceMappingURL=styles-c9e6243fdc0ee2fe4614.js.map
