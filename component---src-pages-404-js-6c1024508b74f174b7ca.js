1861f142b3df3452ebb6b00f393ad426f4a4697d
Anton <toxasava@icloud.com>
2019-07-08 04:46:14 -0400

    Updates

---
+++ component---src-pages-404-js-6c1024508b74f174b7ca.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{157:function(n,e,t){"use strict";t.r(e);var o=t(0),u=t.n(o);e.default=function(){return u.a.createElement("h1",null,"Page not found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-6c1024508b74f174b7ca.js.map
---
+++ component---src-pages-store-index-js-1b805a6a4982cea32a16.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{161:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),r=n(173),l=n(170);t.default=function(){return c.a.createElement(r.a,null,c.a.createElement(l.a,{title:"Instructor"}),c.a.createElement("div",{className:"container",style:{minHeight:"100vh"}},c.a.createElement("h1",{style:{textAlign:"center"}},"Тут будет страница с товаром и всякими штучками")))}}}]);
+//# sourceMappingURL=component---src-pages-store-index-js-1b805a6a4982cea32a16.js.map
