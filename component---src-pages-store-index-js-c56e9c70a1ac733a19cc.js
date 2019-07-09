ffdffe407115d2f64ff55dced6f75424cfc3e377
Anton <toxasava@icloud.com>
2019-07-09 06:19:55 -0400

    Updates

---
+++ component---src-pages-store-index-js-c56e9c70a1ac733a19cc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{161:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a),r=n(172),l=n(171);t.default=function(){return c.a.createElement(r.a,null,c.a.createElement(l.a,{title:"Instructor"}),c.a.createElement("div",{className:"container",style:{minHeight:"100vh"}},c.a.createElement("h1",{style:{textAlign:"center"}},"Тут будет страница с товаром и всякими штучками")))}}}]);
+//# sourceMappingURL=component---src-pages-store-index-js-c56e9c70a1ac733a19cc.js.map
